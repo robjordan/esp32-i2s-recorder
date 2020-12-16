3f82ef9252c9bae1e6a500fd96d1a8dd4baf527b
robjordan <github@jordan-maynard.com>
2020-12-16 16:40:28 +0000

    rename, and remove sample code

--- i2s/main/i2s_recorder.c
+++ i2s/main/i2s_recorder.c
@@ -31,55 +31,6 @@
 
 #define SAMPLE_PER_CYCLE (SAMPLE_RATE/WAVE_FREQ_HZ)
 
-// static void setup_triangle_sine_waves(int bits)
-// {
-//     int *samples_data = malloc(((bits+8)/16)*SAMPLE_PER_CYCLE*4);
-//     unsigned int i, sample_val;
-//     double sin_float, triangle_float, triangle_step = (double) pow(2, bits) / SAMPLE_PER_CYCLE;
-//     size_t i2s_bytes_write = 0;
-
-//     printf("\r\nTest bits=%d free mem=%d, written data=%d\n", bits, esp_get_free_heap_size(), ((bits+8)/16)*SAMPLE_PER_CYCLE*4);
-
-//     triangle_float = -(pow(2, bits)/2 - 1);
-
-//     for(i = 0; i < SAMPLE_PER_CYCLE; i++) {
-//         sin_float = sin(i * 2 * PI / SAMPLE_PER_CYCLE);
-//         if(sin_float >= 0)
-//             triangle_float += triangle_step;
-//         else
-//             triangle_float -= triangle_step;
-
-//         sin_float *= (pow(2, bits)/2 - 1);
-
-//         if (bits == 16) {
-//             sample_val = 0;
-//             sample_val += (short)triangle_float;
-//             sample_val = sample_val << 16;
-//             sample_val += (short) sin_float;
-//             samples_data[i] = sample_val;
-//         } else if (bits == 24) { //1-bytes unused
-//             samples_data[i*2] = ((int) triangle_float) << 8;
-//             samples_data[i*2 + 1] = ((int) sin_float) << 8;
-//         } else {
-//             samples_data[i*2] = ((int) triangle_float);
-//             samples_data[i*2 + 1] = ((int) sin_float);
-//         }
-
-//     }
-
-//     i2s_set_clk(I2S_NUM, SAMPLE_RATE, bits, 2);
-//     //Using push
-//     // for(i = 0; i < SAMPLE_PER_CYCLE; i++) {
-//     //     if (bits == 16)
-//     //         i2s_push_sample(0, &samples_data[i], 100);
-//     //     else
-//     //         i2s_push_sample(0, &samples_data[i*2], 100);
-//     // }
-//     // or write
-//     i2s_write(I2S_NUM, samples_data, ((bits+8)/16)*SAMPLE_PER_CYCLE*4, &i2s_bytes_write, 100);
-
-//     free(samples_data);
-// }
 
 void app_main(void)
 {
@@ -128,15 +79,4 @@ void app_main(void)
         printf("%08x\n", *p);
     }
     
-
-    // int test_bits = 16;
-    // while (1) {
-    //     setup_triangle_sine_waves(test_bits);
-    //     vTaskDelay(5000/portTICK_RATE_MS);
-    //     test_bits += 8;
-    //     if(test_bits > 32)
-    //         test_bits = 16;
-
-    // }
-
 }
